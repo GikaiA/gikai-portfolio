59671322497d323e73771a21e67541d39cdeb514
Gikai Andrews <gnaboys@gmail.com>
2025-09-13 23:36:53 -0400

    added icons, still working on it

--- gikai-portfolio/src/Home/Home.jsx
+++ gikai-portfolio/src/Home/Home.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Home.css";
 import { Typewriter } from "react-simple-typewriter";
 import legendofti from "../images/legendofti.png";
+import { FaReact } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
 
 function Home() {
   return (
@@ -29,26 +33,24 @@ function Home() {
         </div>
       </div>
       <div className="projects" id="projects">
-        <h1 className="projects-title">Projects</h1>
+        <h1 className="title">Projects</h1>
         <div className="projects-section">
-            <div className="card">
-              <img
-                src={legendofti}
-                alt="legendofti-thumbnail"
-                className="image"
-              ></img>
-              <div className="project-bio-section"></div>
-              <p className="project-bio">
-                The LegendOfTi website highlights the content creator,
-                LegendOfTi. This custom portfolio website was designed and
-                developed to highlight the unique style and brand of the
-                creator, integrating social media and sponsor visibility
-                enhancements to maximize engagement and revenue.
-              </p>
-              <div className="icons">
-                
-              </div>
-            </div>
+          <div className="card">
+            <img
+              src={legendofti}
+              alt="legendofti-thumbnail"
+              className="image"
+            ></img>
+            <div className="project-bio-section"></div>
+            <p className="project-bio">
+              The LegendOfTi website highlights the content creator, LegendOfTi.
+              This custom portfolio website was designed and developed to
+              highlight the unique style and brand of the creator, integrating
+              social media and sponsor visibility enhancements to maximize
+              engagement and revenue.
+            </p>
+            <div className="icons"></div>
+          </div>
         </div>{" "}
         <div className="card">
           <img
@@ -72,6 +74,14 @@ function Home() {
             It includes{" "}
           </p>
         </div>{" "}
+      </div>
+      <div className="skills-section">
+        <h1 className="title">Skills</h1>
+        <div className="skills-grid">
+          <FaReact className="skill-icon" />
+          <FaHtml5 className="skill-icon" />
+          <FaCss3Alt className="skill-icon" />
+        </div>
       </div>
     </div>
   );
