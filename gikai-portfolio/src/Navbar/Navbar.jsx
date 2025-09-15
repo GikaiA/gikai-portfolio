4dd3e8ed6c48626cc9bde0365b963475adab61dc
Gikai Andrews <gnaboys@gmail.com>
2025-09-14 22:50:04 -0400

    attempted dark mode

--- gikai-portfolio/src/Navbar/Navbar.jsx
+++ gikai-portfolio/src/Navbar/Navbar.jsx
@@ -3,22 +3,29 @@ import "./Navbar.css";
 
 
 function Navbar() {
-  const [isOpen, setIsOpen] =  useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="navbar">
       <div className="logo">
-        <a href="/" className="logo-link">GA</a>
+        <a href="/" className="logo-link">
+          GA
+        </a>
       </div>
       <ul className={isOpen ? "nav-menu open" : "nav-menu"}>
         <li>
-          <a href="#projects" className="nav-item">Projects</a>
+          <a href="#projects" className="nav-item">
+            Projects
+          </a>
         </li>
         <li>
-          <a href="/services" className="nav-item">About</a>
+          <a href="/services" className="nav-item">
+            About
+          </a>
         </li>
         <li>
-          <a href="#" className="nav-item">Contact</a>
+          <a href="#" className="nav-item">
+            Contact
+          </a>
         </li>
         <div className="close-menu" onClick={() => setIsOpen(false)}>
           ✖
